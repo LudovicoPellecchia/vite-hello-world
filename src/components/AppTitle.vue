35e1573bfcf5fdfb3710e7604c2a0b1dab38e6e3
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-08-22 15:07:35 +0200

    push esercizio

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+    name: 'AppTitle',
+    data(){
+
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<template>
+    <div class="title-wrapper">
+        <h1 class="title">La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style>
+.title{
+    text-align: center;
+    margin: auto;
+    font-size: 3rem;
+}
+</style>
